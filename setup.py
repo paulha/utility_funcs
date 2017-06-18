31559fea38512a76b4a01ce0d888238b6acc04fb
Paul Hanchett <paul.hanchett@gmail.com>
2017-06-17 20:52:48 -0700

    Added test target to pycharm project.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='utility_funcs',
       version='0.1',
       description='Utilities for useful actions that I keep repeating...',
-      url='http://github.com/storborg/funniest',
+      url='http://github.com/paulha/utility_funcs',
       author='Paul Hanchett',
       author_email='paul.hanchett@gmail.com',
       license='MIT',
