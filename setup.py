69305fc7f080b1814a214e9265149c1e6603a8dd
Paul Hanchett <paul.hanchett@gmail.com>
2017-06-18 14:31:44 -0700

    Added install_requirements to setup Corrected github url in setup.

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
+install_requires = {
+      'os',
+      'unittest',
+      'yaml'
+}
+
 setup(name='utility_funcs',
       version='0.1',
       description='Utilities for useful actions that I keep repeating...',
-      url='http://github.com/paulha/utility_funcs',
+      url='https://github.com/paulha/utility_funcs.git',
       author='Paul Hanchett',
       author_email='paul.hanchett@gmail.com',
       license='MIT',
