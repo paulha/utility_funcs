0769ab0fedc7b356b3eeda990904387da0817e03
Paul Hanchett <pfhanchx@intel.com>
2017-07-25 15:17:24 -0700

    many changes to account for bad characters mixed into Jora and JAMA data. :-(
    trim double spaces out of trimmed string to normalize it.

--- utility_funcs/__init__.py
+++ utility_funcs/__init__.py
@@ -1,7 +1,5 @@
-import string
-
 def strip_non_ascii(string):
     """Returns the string without non ASCII characters, L & R trim of spaces"""
     stripped = (c for c in string if c >=' ' and c<='~')
-    return string.replace(''.join(stripped).strip(" \t\n\r"), "  ", " ")
+    return ''.join(stripped).strip(" \t\n\r")replace("  ", " ")
 
